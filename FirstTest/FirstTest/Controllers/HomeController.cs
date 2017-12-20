da86572f23b47bbfcbea00f8e7ef8d0537c96e92
Vova Maslo <vovamaslo@gmail.com>
2017-12-20 10:18:25 +0300

    Ready to show

--- FirstTest/FirstTest/Controllers/HomeController.cs
+++ FirstTest/FirstTest/Controllers/HomeController.cs
@@ -12,32 +12,36 @@ namespace FirstTest.Controllers
     public class HomeController : Controller
     {
         [Route("Index/page/{page?}")]
-        public ActionResult Index(int page = 1)
+        public ActionResult Index(int? page = 1)
         {
+            if (!page.HasValue)
+                page = 1;
             page = (page < 1) ? 1 : page;
             page = (page > 5) ? 5 : page;
-            return View(page);
+            return View(page.Value);
         }
 
         [Route("Data/{page?}")]
         [ChildActionOnly]
         public ActionResult ShowData(int page = 1)
         {
-            List<ImageLinkInfo> Image = new List<ImageLinkInfo>();
+            List<ImageLinkInfo> Images = new List<ImageLinkInfo>();
             using (ImageDB db = new ImageDB())
             {
-                Image = db.ImageLinksInfo.ToList();
+                Images = db.ImageLinksInfo.ToList();
 
-                Image = Image.Skip((page - 1) * 3).Take(3).ToList();
+                Images = Images.Skip((page - 1) * 3).Take(3).ToList();
             }
-                return PartialView(Image);
+            ViewBag.page = page;
+                return PartialView(Images);
         }
 
         [HttpGet]
         [Route("Data/Edit/{id?}")]
         public ActionResult EditData(int? id)
         {
-            if(id == null)
+
+            if(id == null )
                 return HttpNotFound();
 
             using (var db = new ImageDB())
@@ -58,8 +62,58 @@ namespace FirstTest.Controllers
             {
                 db.Entry(model).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", new { page = 1 });
             }
+        }
+
+        [HttpGet]
+        [Route("Data/Delete/{id?}")]
+        public ActionResult DeleteData(int? id)
+        {
+            if (id != null)
+            using (var db = new ImageDB())
+            {
+                var model = db.ImageLinksInfo.Find(id);
+                if (model != null)
+                    return View(model);
+            }
+            return HttpNotFound();
+        }
+
+        [HttpPost, ActionName("DeleteData")]
+        [Route("Data/Delete")]
+        public ActionResult DeleteDataConfirmed(ImageLinkInfo model)
+        {
+            using (var db = new ImageDB())
+            {
+                var modelToDelete = db.ImageLinksInfo.Find(model.Id);
+                if(modelToDelete != null)
+                {
+                    db.ImageLinksInfo.Remove(modelToDelete);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", new { page = 1 });
+                }
+            }
+            return HttpNotFound();
+        }
+
+        [HttpGet]
+        [Route("Data/Add")]
+        public ActionResult AddData()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Data/Add")]
+        public ActionResult AddData(ImageLinkInfo model)
+        {
+            using (var db = new ImageDB())
+            {
+                db.Entry(model).State = EntityState.Added;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index", new { page = 1 });
         }
     }
 }
